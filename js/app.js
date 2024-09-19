5f4e954dc4823a22cc96cba49d90f1a8181b5028
seffmeister <ismail.sefin@gmail.com>
2024-09-19 10:47:50 +0200

    modularity

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
+//Document elements
 const textField0 = document.getElementById("text0");
-textField0.innerHTML = showStylizedScore(7);
+//textField0.innerHTML = showStylizedScore(7);
+const textField1 = document.getElementById("text1");
+
+//Internal variables
+let score = 0;
+
+increaseScoreByOne();
 
 
+// Controllers
+//function showscore(inputNumber) {
+//  return inputNumber * 100;
+//}
+//function showStylizedScore(scoreInput) {
+//  return 'Your Score is bad:' + scoreInput * 100;
+//}
 
-function showscore(inputNumber) {
-  return inputNumber * 100;
+function inreaseScoreByOne() {
+  score++;
+  updateScoreText (score);
 }
 
-
-function showStylizedScore(scoreInput) {
-  return 'Your Score is bad:' + scoreInput * 100;
+// View
+function updateScoreText(newText) {
+  textField0.innerHTML = newText;
+}
+function updateStatusText(newText) {
+  textField1.innerHTML = newText;
 }
